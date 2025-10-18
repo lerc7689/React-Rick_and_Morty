8f437692714a769dc752cd08912d502ffaa6fd60
Luis <lerc7689@hotmail.com>
2025-10-18 18:10:20 -0400

    rickAndMortyRepair

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,8 +8,10 @@ const Footer = () => {
     <>
 
     <div className="footer">
-            <a href="https://wa.me/8297213784?text=¡Estoy+interesado!"><img src={whatsappImage} className="Image"/></a>
-           <a href="https://msng.link/o?@lerc7689=ig"><img src={igImage} className="Image"/></a>
+      <div className="imagesContainer">
+        <a href="https://wa.me/8297213784?text=¡Estoy+interesado!"><img src={whatsappImage} className="Image"/></a>
+        <a href="https://msng.link/o?@lerc7689=ig"><img src={igImage} className="Image"/></a>
+      </div>
      
             <p>Created by Luis E. Ramirez C. 2023®</p>
       </div>
